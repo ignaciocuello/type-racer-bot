d14165dd86f6f8518da355bccee0c43a77574a12
Ignacio Cuello <icuelloreyes97@gmail.com>
2021-01-14 16:17:20 +1100

    Added some TODOs for later study

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const {Builder} = require('selenium-webdriver');
 
+//TODO: understand what async/await do
 (async function openGoogle() {
+    //TODO: look at Builder doco
+    //TODO: look at driver doco
     let driver = await new Builder().forBrowser('chrome').build();
     await driver.get("http://google.com");
 })();
