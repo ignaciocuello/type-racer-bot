483020042d49055f8a6b8ca115ae500302ffb374
Ignacio Cuello <icuelloreyes97@gmail.com>
2021-01-15 19:08:34 +1100

    Separated and refactored the main code block, still yet to break out into functions

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const {Builder, By, until} = require('selenium-webdriver');
 
 //TODO: clean this up
-(async function openGoogle() {
-    let driver = await new Builder().forBrowser('chrome').build();
+(async function runTypeRacerBot() {
+    const driver = await new Builder().forBrowser('chrome').build();
     await driver.get('https://play.typeracer.com/');
-    try {
-      const maxWaitTimeMs = 10000;
-      const buttonContainerSelector = '#gwt-uid-1 > a';
-      await driver.wait(until.elementLocated(By.css(buttonContainerSelector), maxWaitTimeMs));
-      let buttonContainer = await driver.findElement(By.css(buttonContainerSelector));
-      await buttonContainer.click();
-      const textDivSelector = 'table.inputPanel > tbody > tr > td > table > tbody > tr > td > div > div';
 
-      await driver.wait(until.elementLocated(By.css(textDivSelector), maxWaitTimeMs));
-      let textDiv = await driver.findElement(By.css(textDivSelector));
+    const maxWaitTimeMs = 10 * 1000;
 
-      let text = await textDiv.getText();
-      console.log(text);
-    } catch (e) {
-      console.log(e);
-    }
+    const enterRaceButtonSelector = '#gwt-uid-1 > a';
+    await driver.wait(
+      until.elementLocated(By.css(enterRaceButtonSelector), maxWaitTimeMs));
+    const enterRaceButton =
+      await driver.findElement(By.css(enterRaceButtonSelector));
+    await enterRaceButton.click();
+
+    const targetTextDivSelector =
+      'table.inputPanel > tbody > tr > td > table > tbody > tr > td > div > div';
+    await driver.wait(
+      until.elementLocated(By.css(targetTextDivSelector), maxWaitTimeMs));
+    const targetTextDiv =
+      await driver.findElement(By.css(targetTextDivSelector));
+    const targetText = await targetTextDiv.getText();
+
+    console.log(targetText);
 })();
