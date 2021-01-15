89daac1c5723490e7e5c18f679dfce1fc66b5425
Ignacio Cuello <icuelloreyes97@gmail.com>
2021-01-15 15:27:23 +1100

    Now finding start race button and clicking it
    Signed-off-by: Ignacio Cuello <icuelloreyes97@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const {Builder} = require('selenium-webdriver');
+const {Builder, By, until} = require('selenium-webdriver');
 
 (async function openGoogle() {
     let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get("https://play.typeracer.com/");
+    await driver.get('https://play.typeracer.com/');
+    try {
+      const maxWaitTimeMs = 10000;
+      await driver.wait(until.elementLocated(By.id('gwt-uid-1'), maxWaitTimeMs));
+      let buttonContainer = await driver.findElement(By.css('#gwt-uid-1 > a'));
+      await buttonContainer.click();
+      console.log(buttonContainer);
+    } catch (e) {
+      console.log(e);
+    }
 })();
