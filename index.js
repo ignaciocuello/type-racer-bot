5c87c3968c793210612719f762525efc606fc580
Ignacio Cuello <icuelloreyes97@gmail.com>
2021-01-15 19:33:04 +1100

    Cleaned up by breaking main block into functions, still need a lil more cleanup

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 const {Builder, By, until} = require('selenium-webdriver');
 
-//TODO: clean this up
+//TODO: clean this up a little more
 (async function runTypeRacerBot() {
     const driver = await new Builder().forBrowser('chrome').build();
+    
+    //Enter a race in type racer, once it loads get the race's target text.
     await driver.get('https://play.typeracer.com/');
-
-    const maxWaitTimeMs = 10 * 1000;
-
-    const enterRaceButtonSelector = '#gwt-uid-1 > a';
-    await driver.wait(
-      until.elementLocated(By.css(enterRaceButtonSelector), maxWaitTimeMs));
-    const enterRaceButton =
-      await driver.findElement(By.css(enterRaceButtonSelector));
-    await enterRaceButton.click();
-
-    const targetTextDivSelector =
-      'table.inputPanel > tbody > tr > td > table > tbody > tr > td > div > div';
-    await driver.wait(
-      until.elementLocated(By.css(targetTextDivSelector), maxWaitTimeMs));
-    const targetTextDiv =
-      await driver.findElement(By.css(targetTextDivSelector));
-    const targetText = await targetTextDiv.getText();
+    await clickEnterRaceButton(driver);
+    const targetText = await getTargetText(driver);
 
     console.log(targetText);
+
+    async function waitAndFindElement(driver, locator) {
+      const maxWaitTimeMs = 10 * 1000;
+
+      await driver.wait(until.elementLocated(locator, maxWaitTimeMs));
+      const element = await driver.findElement(locator);
+
+      return element;
+    }
+
+    async function clickEnterRaceButton(driver) {
+      const enterRaceButtonSelector = '#gwt-uid-1 > a';
+      const enterRaceButton =
+        await waitAndFindElement(driver, By.css(enterRaceButtonSelector));
+      return await enterRaceButton.click();
+    }
+
+    async function getTargetText(driver) {
+      const targetTextDivSelector =
+        'table.inputPanel > tbody > tr > td > table > tbody > tr > td > div > div';
+      const targetTextDiv =
+        await waitAndFindElement(driver, By.css(targetTextDivSelector));
+      const targetText = await targetTextDiv.getText();
+      return targetText;
+    }
 })();
