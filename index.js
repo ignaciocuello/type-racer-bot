e04ba45965d2ae0414ac19a36753520235e7b7da
Ignacio Cuello <icuelloreyes97@gmail.com>
2021-01-15 16:24:19 +1100

    Finding text element now and storing text as a variable
    Signed-off-by: Ignacio Cuello <icuelloreyes97@gmail.com>

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const {Builder, By, until} = require('selenium-webdriver');
 
+//TODO: clean this up
 (async function openGoogle() {
     let driver = await new Builder().forBrowser('chrome').build();
     await driver.get('https://play.typeracer.com/');
     try {
       const maxWaitTimeMs = 10000;
-      await driver.wait(until.elementLocated(By.id('gwt-uid-1'), maxWaitTimeMs));
-      let buttonContainer = await driver.findElement(By.css('#gwt-uid-1 > a'));
+      const buttonContainerSelector = '#gwt-uid-1 > a';
+      await driver.wait(until.elementLocated(By.css(buttonContainerSelector), maxWaitTimeMs));
+      let buttonContainer = await driver.findElement(By.css(buttonContainerSelector));
       await buttonContainer.click();
-      console.log(buttonContainer);
+      const textDivSelector = 'table.inputPanel > tbody > tr > td > table > tbody > tr > td > div > div';
+
+      await driver.wait(until.elementLocated(By.css(textDivSelector), maxWaitTimeMs));
+      let textDiv = await driver.findElement(By.css(textDivSelector));
+
+      let text = await textDiv.getText();
+      console.log(text);
     } catch (e) {
       console.log(e);
     }
