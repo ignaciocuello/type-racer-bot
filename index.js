1167b919cbe01aa3c6a5dafacbc23ec0af8eee10
Ignacio Cuello <icuelloreyes97@gmail.com>
2021-01-17 18:55:25 +1100

    Fixed try finally block since driver was undefined

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const {Builder, By, until} = require('selenium-webdriver');
 
-//TODO: clean this up a little more
 (async function runTypeRacerBot() {
-    const driver = await new Builder().forBrowser('chrome').build();
-    
+  const driver = await new Builder().forBrowser('chrome').build();
+
+  try {
     await driver.get('https://play.typeracer.com/');
     await enterRace(driver);
     const raceTargetText = await getRaceTargetText(driver);
 
     console.log(raceTargetText);
 
-    async function enterRace(driver) {
-      const enterRaceButton =
-        await driver.wait(until.elementLocated(By.css('#gwt-uid-1 > a')));
-      return await enterRaceButton.click();
-    }
+  } finally {
+    await driver.quit();
+  }
 
-    async function getRaceTargetText(driver) {
-      const raceTargetTextDivSelector =
-        'table.inputPanel > tbody > tr > td > table > tbody > tr > td > div > div';
-      const raceTargetTextDiv =
-        await driver.wait(until.elementLocated(By.css(raceTargetTextDivSelector)));
-      const raceTargetText = await raceTargetTextDiv.getText();
-      return raceTargetText;
-    }
+  async function enterRace(driver) {
+    const enterRaceButton =
+      await driver.wait(until.elementLocated(By.css('#gwt-uid-1 > a')));
+    return await enterRaceButton.click();
+  }
+
+  async function getRaceTargetText(driver) {
+    const raceTargetTextDivSelector =
+      'table.inputPanel > tbody > tr > td > table > tbody > tr > td > div > div';
+    const raceTargetTextDiv =
+      await driver.wait(until.elementLocated(By.css(raceTargetTextDivSelector)));
+    const raceTargetText = await raceTargetTextDiv.getText();
+    return raceTargetText;
+  }
 })();
